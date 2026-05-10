f618b8341d44c94acd51ee82235c6311899cbaf5
karthik <kurvakarthikpvk@gmail.com>
2026-05-10 13:45:57 +0530

    fixed keras model loading

--- disease_detection/ml/model.py
+++ disease_detection/ml/model.py
@@ -1,6 +1,11 @@
 from tensorflow.keras.models import load_model
+import keras
 import os
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "rice_model.h5")
 
-model = load_model(MODEL_PATH, compile=False)
+model = load_model(
+    MODEL_PATH,
+    compile=False,
+    safe_mode=False
+)
