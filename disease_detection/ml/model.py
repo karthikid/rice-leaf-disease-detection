c944acc5188158a98bebe25cf8289bfd1c4602ac
karthik <kurvakarthikpvk@gmail.com>
2026-05-10 13:52:46 +0530

    final render fix

--- disease_detection/ml/model.py
+++ disease_detection/ml/model.py
@@ -1,11 +1,24 @@
-from tensorflow.keras.models import load_model
-import keras
 import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Dense
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "rice_model.h5")
+# ---- Fix for Keras quantization_config issue ----
+original_from_config = Dense.from_config
+
+@classmethod
+def patched_from_config(cls, config):
+    config.pop("quantization_config", None)
+    return original_from_config(config)
+
+Dense.from_config = patched_from_config
+
+# ---- Load Model ----
+MODEL_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "rice_model.h5"
+)
 
 model = load_model(
     MODEL_PATH,
-    compile=False,
-    safe_mode=False
+    compile=False
 )
